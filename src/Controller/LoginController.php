b871500012d515854b137d5049b4d9bc71f2f0d3
Martin Růžek <mata.ruzek@gmail.com>
2021-02-25 20:03:58 +0100

    25.2.2021 Update

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -22,9 +22,10 @@ class LoginController extends AbstractController
     {
         // set the base url for the SSO application
         $ssoUrlBase = "https://titan.spsostrov.cz/ssogw/";
-
+        // Normal version
         $callbackUrl = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REDIRECT_URL'];
-        //$callbackUrl = $protocol . "://localhost:8081/login";
+        // Wedos version
+        //$callbackUrl = "https://dmp.martinruzek.eu/login";
 
         #create the service token
         $service = base64_encode($callbackUrl);
@@ -45,6 +46,18 @@ class LoginController extends AbstractController
 
             //get the user data
             $data = file_get_contents($ssoUserDataRequestingUrl);
+
+            $responseFile = file_get_contents(__DIR__ . '/../response.json');
+            $json = json_decode($responseFile, true);
+
+            $newResponse = [
+                'date' => date('Y-m-d H:i:s'),
+                'response' => $data
+            ];
+
+            array_push($json, $newResponse);
+            $json = json_encode($json);
+            file_put_contents(__DIR__ . '/../response.json', $json);
 
             $strposLogin = strpos($data, "login");
             $strposName = strpos($data, "name");
@@ -152,35 +165,6 @@ class LoginController extends AbstractController
             $this->session->set('tag', $tag);
             $this->session->set('id', $user->getId());
             $this->session->set('user', $user);
-
-            $responseFile = file_get_contents(__DIR__ . '/../response.json');
-            $json = json_decode($responseFile, true);
-
-            $newResponse = [
-                'username' => $username,
-                'group' => $expOu[1],
-                'response' => $data
-            ];
-            $alreadyInFile = false;
-            if ($json != null) {
-                foreach ($json as $user) {
-                    if ($user['username']) {
-                        if ($user['username'] != $username) {
-                            $alreadyInFile = true;
-                            break;
-                        }
-                    }
-                }
-            } else {
-                $alreadyInFile = true;
-            }
-
-
-            if ($alreadyInFile) {
-                array_push($json, $newResponse);
-                $json = json_encode($json);
-                file_put_contents(__DIR__ . '/../response.json', $json);
-            }
 
             return $this->redirect('/');
         } else {
